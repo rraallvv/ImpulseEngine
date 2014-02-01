22b8a1b3805e8f9eb4637357d37198c5e8444094
Developer <>
2014-02-01 19:26:09 -0430

    Add Xcode project

--- glut.h
+++ glut.h
@@ -134,8 +134,14 @@ typedef unsigned short wchar_t;
 
 #endif  /* _WIN32 */
 
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
+#endif /* __APPLE__ */
 
 #ifdef __cplusplus
 extern "C" {
